2aa7d2bb9136e37b486bc2e68f08393a2d1a5e03
Lucas Wolfgramm <lucas.wol@hotmail.com>
2022-07-22 09:18:54 -0300

    Aula 02 - Codigo aula + desafios iniciais

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,5 +1,7 @@
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -31,15 +33,29 @@ public class App {
         //List<Map<String, String>> body = getJson("https://imdb-api.com/en/API/Top250Movies/", token);
         List<Map<String, String>> body = getJson("https://api.mocki.io/v2/549a5d8b", "");
 
+        var geradorFigurinhas = new GeradorFigurinhas();
+
         for (Map<String, String> filme : body) {
+            String urlImagem = filme.get("image");
+            InputStream inputStream = new URL(urlImagem).openStream();
+
+            String nomeArquivo = filme.get("title");
+
+            
             System.out.println("Titulo: " + filme.get("title"));
             System.out.println("Imagem: " + filme.get("image"));
             
             int rating = Math.round(Float.parseFloat(filme.get("imDbRating")));
             System.out.print("Avaliacao: ");
+            String texto = "";
             for (int star = 0; star < rating; star++) {
-                System.out.print("⭐");                
+                System.out.print("⭐"); 
+                texto = texto + "⭐";
+                               
             }
+
+            geradorFigurinhas.criar(inputStream, nomeArquivo, texto);
+
             System.out.println();
             System.out.println("-".repeat(30));
         }
@@ -54,3 +70,8 @@ public class App {
         listaTop250Filmes();
     }
 }
+
+//TO-DO
+//- Mudar jsonParser para uma biblioteca jackson ou gson
+//- Criar forma de avaliar
+//-- Buscar de um arquivo de config ou digitado
---
+++ alura-stickers/src/GeradorFigurinhas.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class GeradorFigurinhas {
+
+    public void criar(InputStream inputStream, String nomeArquivo, String texto) throws Exception {
+        
+        //InputStream inputStream = new FileInputStream(new File("src/img/shawshankRedemption.jpg"));
+        //InputStream inputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg").openStream();
+        
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+
+        int novaAltura = altura + 200;
+
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, imagemOriginal.TRANSLUCENT);
+
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+        
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.red);
+        graphics.setFont(fonte);
+
+        graphics.drawString("Classificação:", (int) (largura * 0.2), novaAltura - 100);
+        graphics.drawString(texto, (int) (largura * 0.4), novaAltura - 50);
+
+        
+        // Stroke strokeOriginal = graphics.getStroke();
+        graphics.setColor(Color.yellow);
+        graphics.setStroke(new BasicStroke(10));
+        graphics.drawRect(0, 0, largura, novaAltura);
+        // graphics.setStroke(strokeOriginal);
+
+        ImageIO.write(novaImagem, "png", new File("src/img/" + nomeArquivo + ".png"));
+    }
+}
